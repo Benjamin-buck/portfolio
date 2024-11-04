fbc9b28aa36ec7c08a649d095c3ad4f4bd7dc7b3
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-11-04 16:45:12 -0500

    Fixes portfolio responsiveness.

--- constants/projects/GitHub.ts
+++ constants/projects/GitHub.ts
@@ -1,7 +1,21 @@
 export const GitHub = {
   title: "GitHub",
-  description: "This is a project.",
+  description:
+    "This project was a challenge for myself to dive deep into user interface development by re-creating the current github external and internal interface.",
   status: "In Progress",
   image: "/github.png",
   technology: ["HTML", "CSS", "JavaScript"],
+  github: "https://github.com/Benjamin-buck/github-clone",
+  features: [
+    {
+      label: "Mobile Responsive",
+      description:
+        "The app is completely mobile responsive and seamlessly collapses according to the size of the device.",
+    },
+    {
+      label: "Pixel Perfect",
+      description:
+        "The project very closely replicates the current user interface and user interaction with a fine attention to detail.",
+    },
+  ],
 };
