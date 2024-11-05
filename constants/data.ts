83a3376dfcd2224f4893f1d5ad220628aaaeca9c
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-11-04 19:41:44 -0500

    Adds content to the about page.

--- constants/data.ts
+++ constants/data.ts
@@ -1,6 +1,5 @@
 export const navigationLinks = [
   { label: "Home", href: "/", icon: "/home.png" },
   { label: "About", href: "/about", icon: "/home.png" },
-  { label: "Blog", href: "/blog", icon: "/home.png" },
   { label: "Resume", href: "/resume", icon: "/home.png" },
 ];
