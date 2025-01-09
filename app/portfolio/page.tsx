6089886f9b194508920273f79d93a7cdbcdd238c
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2025-01-09 15:29:23 -0500

    Adds loading page to aroseforann

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,9 +1,17 @@
+"use client";
 import PortfolioCards from "@/components/cards/PortfolioCards";
 import { portfolio } from "@/constants/portfolio";
 import { allProjects } from "@/constants/projects/AllProjects";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ClipLoader } from "react-spinners";
 
-const page = async () => {
+const page = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
   return (
     <main className="mb-8">
       <div className="mt-12 flex justify-between items-center border-b pb-5 max-sm:mx-8 max-sm:flex-col max-sm:items-start max-sm: gap-2">
@@ -22,20 +30,25 @@ const page = async () => {
         </div>
       </div>
 
-      {/* Cards grid */}
-      <div className="grid grid-cols-4 gap-2 mt-4 max-md:grid-cols-2 max-sm:grid-cols-1 max-sm:mx-8 max-sm:gap-8">
-        {allProjects.map((project) => (
-          <PortfolioCards
-            label={project.title}
-            key={project.title}
-            image={project.image}
-            href={project.href}
-            description={project.description}
-            status={project.status}
-            disabled={project.disabled}
-          />
-        ))}
-      </div>
+      {isLoading ? (
+        <div className="w-full h-[60vh] flex items-center justify-center">
+          <ClipLoader />
+        </div>
+      ) : (
+        <div className="grid grid-cols-4 gap-2 mt-4 max-md:grid-cols-2 max-sm:grid-cols-1 max-sm:mx-8 max-sm:gap-8">
+          {allProjects.map((project) => (
+            <PortfolioCards
+              label={project.title}
+              key={project.title}
+              image={project.image}
+              href={project.href}
+              description={project.description}
+              status={project.status}
+              disabled={project.disabled}
+            />
+          ))}
+        </div>
+      )}
     </main>
   );
 };
