2a012c97b3668d1ce65180c4244eaca7970a2af7
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2025-02-19 09:12:24 -0500

    Update page.jsx

--- app/portfolio/[id]/page.jsx
+++ app/portfolio/[id]/page.jsx
@@ -31,8 +31,12 @@ const IndividualProjectPage = () => {
   return (
     <div className="mx-4 mb-8">
       {isModalOpen && (
-        <Modal onClose={handleClick} isOpen={isModalOpen} title="Modal Title">
-          <div className="grid grid-cols-2 gap-3">
+        <Modal
+          title="Project Images"
+          onClose={handleClick}
+          isOpen={isModalOpen}
+        >
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-3">
             {project.images.map((image) => (
               <Image
                 key={image}
